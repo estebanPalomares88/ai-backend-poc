d589121040b2a78a2c8802f593c424c852af8fd2
Jaime Palomares <estebanpalomares23@gmail.com>
2025-02-20 09:47:04 -0500

    swagger implemented

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask_cors import CORS
 from src.main.rest.InfoEndpoint import info_endpoint
+from flasgger import Swagger
 
 app = Flask(__name__)
 CORS(app)
+swagger = Swagger(app)
 
 app.register_blueprint(info_endpoint)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return redirect('/apidocs')
 
 if __name__ == '__main__':
     app.run()
--- src/main/rest/InfoEndpoint.py
+++ src/main/rest/InfoEndpoint.py
@@ -4,22 +4,46 @@ import geocoder
 import pytz
 from timezonefinder import TimezoneFinder
 import secrets
+from flasgger import swag_from
 
 
 info_endpoint = Blueprint('info_endpoint', __name__)
 
 
 @info_endpoint.route('/info', methods=['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+@swag_from({
+    'responses': {
+        200: {
+            'description': 'Successful response',
+            'examples': {
+                'application/json': {
+                    "method": "GET",
+                    "date": "2025-02-20T12:34:56.789Z",
+                    "server time zone": "UTC",
+                    "server location": "New York",
+                    "request ip": "192.168.1.1",
+                    "token genrated": "abcdef1234567890"
+                }
+            }
+        },
+        500: {
+            'description': 'Internal server error',
+            'examples': {
+                'application/json': {
+                    "error": "Error message"
+                }
+            }
+        }
+    }
+})
 def info():
     try:
         location = geocoder.ip('me').city
         latlng = geocoder.ip('me').latlng
         timezone_str = TimezoneFinder().timezone_at(lng=latlng[1], lat=latlng[0])
         timezone = pytz.timezone(timezone_str)
-        print(timezone)
         date = datetime.datetime.now(pytz.utc).astimezone(timezone)
         tz = date.tzname()
-        print(tz)
         request_ip = request.remote_addr
         token = secrets.token_hex(16)
 
