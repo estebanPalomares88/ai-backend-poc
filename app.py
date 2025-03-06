cd3a8cf148a50ef6eb07639e8b1587767e83b014
estebanPalomares88 <154276638+estebanPalomares88@users.noreply.github.com>
2025-03-06 10:19:28 -0500

    Message endpoint implemented (#3)
    * swagger implemented
    
    * swagger first approach
    
    * message endpoint created
    
    * Message endpoint added

--- app.py
+++ app.py
@@ -1,15 +1,43 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask_cors import CORS
 from src.main.rest.InfoEndpoint import info_endpoint
+from src.main.rest.MessageEndpoint import message_endpoint 
+from flasgger import Swagger
 
 app = Flask(__name__)
 CORS(app)
 
+swagger_config = {
+    "swagger": "2.0",
+    "info": {
+        "title": "Test Backend PoC",
+        "description": "This is the backend for a proof of concept of testing APIs with different functionalities",
+        "version": "1.0.0"
+    },
+    "specs": [
+        {
+            "endpoint": 'apispec_1',
+            "route": '/apispec_1.json',
+            "rule_filter": lambda rule: True,  
+            "model_filter": lambda tag: True,  
+        }
+    ],
+    "headers": [], 
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/apidocs/",
+    "title" : "Test Backend",
+    'hide_top_bar': True
+}
+
+swagger = Swagger(app, config=swagger_config)
+
 app.register_blueprint(info_endpoint)
+app.register_blueprint(message_endpoint) 
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return redirect('/apidocs')
 
 if __name__ == '__main__':
     app.run()
--- src/main/rest/InfoEndpoint.py
+++ src/main/rest/InfoEndpoint.py
@@ -4,22 +4,47 @@ import geocoder
 import pytz
 from timezonefinder import TimezoneFinder
 import secrets
+from flasgger import swag_from
 
 
 info_endpoint = Blueprint('info_endpoint', __name__)
 
 
-@info_endpoint.route('/info', methods=['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+@info_endpoint.route('/info', methods=['GET'])
+@swag_from({
+    'tags': ['Info'],
+    'responses': {
+        200: {
+            'description': 'Successful response',
+            'examples': {
+                'application/json': {
+                    "method": "GET",
+                    "date": "2025-02-20T12:34:56.789Z",
+                    "server time zone": "UTC",
+                    "server location": "New York",
+                    "request ip": "192.168.1.1",
+                    "token genrated": "abcdef1234567890"
+                }
+            }
+        },
+        500: {
+            'description': 'Internal server error',
+            'examples': {
+                'application/json': {
+                    "error": "Error message"
+                }
+            }
+        }
+    }
+})
 def info():
     try:
         location = geocoder.ip('me').city
         latlng = geocoder.ip('me').latlng
         timezone_str = TimezoneFinder().timezone_at(lng=latlng[1], lat=latlng[0])
         timezone = pytz.timezone(timezone_str)
-        print(timezone)
         date = datetime.datetime.now(pytz.utc).astimezone(timezone)
         tz = date.tzname()
-        print(tz)
         request_ip = request.remote_addr
         token = secrets.token_hex(16)
 
@@ -31,11 +56,6 @@ def info():
             "request ip" : request_ip,
             "token genrated" : token
         }
-
-        if not request.data:
-            return response, 200
-        data = request.json
-        response["data"] = data
         return response, 200
     except Exception as e:
         return {"error" : str(e)}, 500
